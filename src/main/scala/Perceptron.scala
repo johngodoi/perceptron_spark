8e6bab5254ad5efa9d384b93acb1dd9b8c7c2c16
John Henrique Teixeira de Godoi <john.godoi@gmail.com>
2017-05-28 13:33:36 -0300

    working

--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -4,12 +4,47 @@ import org.apache.commons.math3.analysis.function.Sigmoid
   * @author john
   * @since 25/05/17
   */
-class Perceptron (n:Double, weight:List[Double], teta:Double) {
+class Perceptron (learningRate:Double, weights:Array[Double], teta:Double, acceptedError:Double, iterations:Int) {
 
   val activationFunction = new Sigmoid()
+  var error=Double.MaxValue
 
-  def train(sample:List[Double])={
-    activationFunction.value(cartesianProduct(sample,weight)-teta)
+  def train(samples:List[(Double,List[Double])]):AnyRef={
+    var internalWeights=weights
+    var i = 1
+    do {
+      println(s"epoch ${i}")
+      println(s"error ${error} before epoch")
+      println("weights before epoch: "+internalWeights.mkString(","))
+      samples.foreach(sample => sample match {
+        case (eo, ss) => {
+          internalWeights=trainForOneSample(ss, eo, internalWeights)
+        }
+      })
+      println(s"epoch ${i}")
+      println(s"error ${error} after epoch")
+      println("weights after epoch: "+internalWeights.mkString(","))
+      i=i+1
+    } while (i<=iterations && Math.abs(error) >= acceptedError)
+    AnyRef
+  }
+
+  def trainForOneSample(sample:List[Double], expectedOutput:Double, weightsP:Array[Double]):Array[Double]={
+
+    val activationValue = activationFunction.value(cartesianProduct(sample,weightsP.toList)-teta)
+    println(s"expectedOutput: ${expectedOutput}")
+    println(s"activationValue: ${activationValue}")
+    println("weights before update: "+weightsP.mkString(","))
+    error = expectedOutput-activationValue
+    if(Math.abs(error)>=acceptedError){
+      for(i <- 0 to weightsP.length-1) {
+        weightsP(i) = {
+          weightsP(i)+learningRate*error*expectedOutput
+        }
+      }
+    }
+    println("Updated weights: "+weightsP.mkString(","))
+    weightsP
   }
 
   def cartesianProduct(sample: List[Double], weight:List[Double]): Double = {
@@ -18,4 +53,6 @@ class Perceptron (n:Double, weight:List[Double], teta:Double) {
       case (Nil,Nil) => 0
     }
   }
+
+
 }
--- src/main/scala/Starter.scala
+++ src/main/scala/Starter.scala
@@ -12,6 +12,35 @@ object Starter extends App {
   val squared = nums.map(x=>x*x)
 
   println(squared.collect().mkString(","))
-  val file = sc.textFile("Conj_dados_exercicio.csv")
-  println(file.filter(x => !x.isEmpty).filter(x => x.contains(":=")).map(x=>x.split(":=")(1).trim).collect().mkString(","))
+  val file = sc.textFile("train.csv")
+
+
+  val list = file.map(line => convertToDoubleList(line)).collect().toList
+  //list.foreach(line => println(line.mkString(",")))
+  val tuples = list.filter(line => line != Nil)
+    .map(line => line match {
+    case (y :: xs) => (y, xs)
+  }).toList
+
+  val perceptron = new Perceptron(learningRate = 0.01,weights=Array(0.1,-0.1,0.1,0.1,-0.1,0.1,0.1,-0.1,-0.1,0.1,0.1),teta=0.01,acceptedError=0.000002,iterations=1000)
+  perceptron.train(tuples)
+
+  def convertToDoubleList(line: String): List[Double] = {
+    val split = line.split(",")
+    var doubleList = List[Double]()
+    for(i <- 0 to split.length-1){
+      try {
+        val d = split(i).toDouble
+        doubleList=d::doubleList
+      } catch {
+        case nfe: NumberFormatException => {
+          println(nfe.toString)
+        }
+      }
+    }
+    println(doubleList.length)
+    println(doubleList.mkString(","))
+    doubleList
+  }
+
 }
